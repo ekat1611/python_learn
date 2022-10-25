925362e5292518ba0554ee6b9229e70efa5812f5
Ekaterina Shiganova <eshiganova@aicorner.org>
2022-10-25 18:31:24 +0400

    add tasks from course Автоматизация тестирования на Python

--- SeleniumLearn/introduction.py
+++ SeleniumLearn/introduction.py
@@ -2,6 +2,7 @@ import time
 
 # webdriver это и есть набор команд для управления браузером
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # инициализируем драйвер браузера. После этой команды вы должны увидеть новое открытое окно браузера
 driver = webdriver.Chrome()
@@ -15,14 +16,14 @@ time.sleep(5)
 
 # Метод find_element_by_css_selector позволяет найти нужный элемент на сайте, указав путь к нему. Способы поиска элементов мы обсудим позже
 # Ищем поле для ввода текста
-textarea = driver.find_element_by_css_selector(".textarea")
+textarea = driver.find_element(By.CSS_SELECTOR, ".textarea")
 
 # Напишем текст ответа в найденное поле
 textarea.send_keys("get()")
 time.sleep(5)
 
 # Найдем кнопку, которая отправляет введенное решение
-submit_button = driver.find_element_by_css_selector(".submit-submission")
+submit_button = driver.find_element(By.CSS_SELECTOR, ".submit-submission")
 
 # Скажем драйверу, что нужно нажать на кнопку. После этой команды мы должны увидеть сообщение о правильном ответе
 submit_button.click()

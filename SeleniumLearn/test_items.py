32fe17c83ecc82dc28221b556dd92a3422237254
Ekaterina Shiganova <eshiganova@aicorner.org>
2022-11-23 20:02:30 +0400

    fix assert

--- SeleniumLearn/test_items.py
+++ SeleniumLearn/test_items.py
@@ -7,7 +7,6 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 # запуск командой pytest -s -v --language=es test_items.py
 def test_exist_button_add_to_basket(browser_change_language):
     browser_change_language.get(link)
-    button_add_to_basket = browser_change_language.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
-    assert button_add_to_basket is not None
+    assert browser_change_language.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
     # визуально оценить язык:
     sleep(5)

5996cdda0e97abb4d38fe354f7f09df821ca8353
Ekaterina Shiganova <eshiganova@aicorner.org>
2022-11-23 14:57:07 +0400

    add file test_items.py for lesson 3.6.10 "Автоматизация тестирования с помощью Selenium и Python"

---
+++ SeleniumLearn/test_items.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.common.by import By
+from time import sleep
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+
+# запуск командой pytest -s -v --language=es test_items.py
+def test_exist_button_add_to_basket(browser_change_language):
+    browser_change_language.get(link)
+    button_add_to_basket = browser_change_language.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
+    assert button_add_to_basket is not None
+    # визуально оценить язык:
+    sleep(5)
